94dad9b2631c4d38c2016bb59051365eb8fa54e1
Jordan Vidrine <jordan@jordanvidrine.com>
2025-04-15 16:15:21 -0500

    fix

--- javascripts/discourse/api-initializers/peak-mode-composer.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import peekModeToggle from "../components/peek-mode-toggle";
-
-export default apiInitializer("1.8.0", (api) => {
-  api.renderInOutlet("before-composer-toggles", peekModeToggle);
-});
